77ddeaf23971704c689966cc7ef7a69359892807
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-05-19 23:08:32 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-  const [meuk, setMeuk] = useState(false);
+  const [on, setOn] = useState(false);
+
+  function save(event) {
+    event.preventDefault();
+    console.log("Let's save the on value: " + on);
+  }
+
   return (
-    <div>
-      {meuk ? "ON" : "OFF"}
-      <hr />
-      <Toggle render={renderToggleButton} />
-      <hr />
-      <Toggle render={renderToggleCheckbox} />
-    </div>
+    <form onSubmit={save}>
+      <Toggle render={renderToggleCheckbox} on={on} toggle={setOn} />
+      <br />
+      <button type="submit">Save</button>
+    </form>
   );
 }
 
-function Toggle(props) {
-  const [on, setOn] = useState(false);
-
-  return <>{props.render(() => setOn(!on), on)}</>;
+function Toggle({ render, toggle, on }) {
+  return <>{render(() => toggle(!on), on)}</>;
 }
 
-function renderToggleButton(toggle, on, bla) {
-  function handleClick() {
-    toggle();
-    bla(on);
-  }
-  return <button onClick={handleClick}>{on ? "ON" : "OFF"}</button>;
-}
-
-function renderToggleCheckbox(toggle, on, bla) {
+function renderToggleCheckbox(toggle, on) {
   return (
     <>
       <input type="checkbox" checked={on} onChange={() => toggle()} />
